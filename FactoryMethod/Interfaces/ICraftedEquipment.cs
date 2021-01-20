5183c2cdd8c7ab75ee38d49e8b9021a1304b5f90
Nizameddin_Kale <nizameddinkale@gmail.com>
2021-01-20 14:47:31 +0300

    Variables with private set

---
+++ FactoryMethod/Interfaces/ICraftedEquipment.cs
@@ -0,0 +1,12 @@
+﻿namespace FactoryMethod
+{
+    public interface ICraftedEquipment
+    {
+        EquipmentType EquipmentType { get; }
+        int Id { get; }
+        string ItemName { get; }
+        float Rating { get; }
+        float Weight { get; }
+        float Value { get; }
+    }
+}
