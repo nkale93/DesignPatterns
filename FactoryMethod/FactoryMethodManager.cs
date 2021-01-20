93a31813cb521435263ef4e5a40760b707256e29
Nizameddin_Kale <nizameddinkale@gmail.com>
2021-01-20 14:48:25 +0300

    Factory Method

---
+++ FactoryMethod/FactoryMethodManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    public class FactoryMethodManager
+    {
+        public IEquipment GetEquipment(EquipmentType equipmentType)
+        {
+            if (equipmentType == EquipmentType.Weapon)
+            {
+                return new Weapon(GetItem(equipmentType));
+            }
+            else if (equipmentType == EquipmentType.Armour)
+            {
+                return new Armour(GetItem(equipmentType));
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private ICraftedEquipment GetItem(EquipmentType equipmentType)
+        {
+            return new CraftedEquipment(equipmentType);
+        }
+    }
+}
